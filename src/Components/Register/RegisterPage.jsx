d5dbe442e5d100a7ee5ae96ca807034db0967b75
HARIHARAN.M <harimahe.1317@gmail.com>
2024-08-23 14:11:27 +0530

    second update

---
+++ src/Components/Register/RegisterPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function RegisterPage() {
+    return (
+        <div className='register-page'>
+            <div className="register-nav">
+                <button>Candidate</button>
+                <button>Company</button>
+            </div>
+            <div className="register-main">
+                <div className="register-form-content">
+                    <form>
+                        <div>
+                            <label htmlFor="">First Name</label>
+                            <input type="text" name="" id="" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
