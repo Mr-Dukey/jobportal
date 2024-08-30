ab53fd8f2078ac1f7253a88066b3e31c58f18c4d
HARIHARAN.M <harimahe.1317@gmail.com>
2024-08-30 18:21:53 +0530

    new update

--- src/Components/Register/RegisterPage.jsx
+++ src/Components/Register/RegisterPage.jsx
@@ -1,25 +1,88 @@
 import React from 'react';
 import './registerpage.css'
-import { TextField, Grid, InputAdornment, Container, FormGroup, FormControlLabel } from '@mui/material';
-import { AccountCircleOutlined, CheckBox, EmailOutlined, PasswordOutlined, RemoveRedEye } from '@mui/icons-material';
+import { Col, Container, Form, Row } from 'react-bootstrap';
 export default function RegisterPage() {
+
+
     return (
-        <div className='register-page'>
+        <div className='reg-page'>
             <div className="reg-page-header">
                 <h1>Register Account</h1>
             </div>
 
             <Container>
 
-                <div className="register-nav">
+                <div className="reg-nav">
                     <button>Candidate</button>
                     <button>Company</button>
                 </div>
-                <div className="register-main">
-                    <div className="register-form-content">
-                        <form>
-                            
-                        </form>
+                <div className="reg-main">
+                    <div className="reg-form-content">
+                        <Form>
+                            <Row>
+                                <Col xs={12} md={6}>
+                                    <Form.Label htmlFor='fName' className='reg-form-label'>First Name <span>*</span></Form.Label>
+                                    <div className="input-area">
+                                        <div className="reg-inp-icon">
+                                            <i class="bi bi-person-circle"></i>
+                                        </div>
+                                        <input type="text" name="" className='reg-inp' id="fName" />
+                                    </div>
+                                </Col>
+                                <Col xs={12} md={6}>
+                                    <Form.Label htmlFor='lName' className='reg-form-label'>Last Name <span>*</span></Form.Label>
+                                    <div className="input-area">
+                                        <div className="reg-inp-icon">
+                                            <i class="bi bi-person-circle"></i>
+                                        </div>
+                                        <input type="text" name="" className='reg-inp' id="lName" />
+                                    </div>
+                                </Col>
+                                <Col xs={12} md={6}>
+                                    <Form.Label htmlFor='uName' className='reg-form-label'>Username <span>*</span></Form.Label>
+                                    <div className="input-area">
+                                        <div className="reg-inp-icon">
+                                            <i class="bi bi-person-circle"></i>
+                                        </div>
+                                        <input type="text" name="" className='reg-inp' id="uName" />
+                                    </div>
+                                </Col>
+                                <Col xs={12} md={6}>
+                                    <Form.Label htmlFor='uEmail' className='reg-form-label'>Email <span>*</span></Form.Label>
+                                    <div className="input-area">
+                                        <div className="reg-inp-icon">
+                                            <i class="bi bi-envelope"></i>
+                                        </div>
+                                        <input type="text" name="" className='reg-inp' id="uEmail" />
+                                    </div>
+                                </Col>
+                                <Col xs={12} md={6}>
+                                    <Form.Label htmlFor='uPass' className='reg-form-label'>Password <span>*</span></Form.Label>
+                                    <div className="input-area">
+                                        <div className="reg-inp-icon">
+                                            <i class="bi bi-envelope"></i>
+                                        </div>
+                                        <input type="text" name="" className='reg-inp' id="uPass" />
+                                    </div>
+                                </Col>
+                                <Col xs={12} md={6}>
+                                    <Form.Label htmlFor='ucPass' className='reg-form-label'>Confirm Password <span>*</span></Form.Label>
+                                    <div className="input-area">
+                                        <div className="reg-inp-icon">
+                                            <i class="bi bi-envelope"></i>
+                                        </div>
+                                        <input type="text" name="" className='reg-inp' id="ucPass" />
+                                    </div>
+                                </Col>
+                                <Col xs={12} className='mt-4 reg-agree'>
+                                    <input type="checkbox" name="" id="reg-check" />&emsp;
+                                    <Form.Label>Here, I will agree company terms & conditions</Form.Label>
+                                </Col>
+                                <Col xs={12} className='mt-4'>
+                                    <button type="submit" className="reg-submit-button">Signup</button>
+                                </Col>
+                            </Row>
+                        </Form>
                     </div>
                 </div>
             </Container>
--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import RegisterPage from './Components/Register/RegisterPage'
+import LoginPage from './Components/Login/LoginPage'
 
 export default function Routing() {
   return (
     <div>
         <Routes>
+          <Route path='/' element={<Navigate to={'login'}/>} />
             <Route path='register' Component={RegisterPage} />
+            <Route path='login' Component={LoginPage} />
         </Routes>
     </div>
   )
