df73b4295390212e3f805f211311e19a40ec1cab
HARIHARAN.M <harimahe.1317@gmail.com>
2024-09-16 10:24:22 +0530

    dashboard update

--- src/Components/CompanyMainDashboard/CompanyDashboard.jsx
+++ src/Components/CompanyMainDashboard/CompanyDashboard.jsx
@@ -6,56 +6,22 @@ import { Link, Outlet } from 'react-router-dom';
 
 
 export default function CompanyDashboard() {
-
-    const [menu, setMenu] = useState(false);
-    const menuRef = useRef();
-
-    function handleToggleMenu() {
-        if (menu === false) {
-            setMenu(true);
-        }
-        else if (menu === true) {
-            setMenu(false)
-        }
-    }
-
-    console.log(menuRef.current);
-
-    useEffect(() => {
-        if (window.innerWidth === 768) {
-            if (menu === false) {
-                menuRef.current.classList.add('menu-inactive');
-                menuRef.current.classList.remove('menu-active');
-            }
-            else if (menu === true) {
-                menuRef.current.classList.add('menu-active');
-                menuRef.current.classList.remove('menu-inactive');
-            }
-        }
-        console.log(menu);
-        
-    }, [menu])
-
-
     return (
         <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
         >
-            <button className='company-main-dashboard-menu' onClick={handleToggleMenu}>
-                <span class="material-symbols-outlined" id='main-icon'>
-                    menu
-                </span>
-                Menu
-            </button>
             <div className='company-main-dashboard'>
-                <div className="company-main-dashboard-sidebar" ref={menuRef}>
+                <div className="company-main-dashboard-sidebar">
                     <SideBarComponent />
                 </div>
                 <div className="company-main-dashboard-outlet">
                     <Outlet />
                 </div>
+            </div>
+            <div className="bottom-bar">
+                <BottomNavigation />
             </div>
         </motion.div>
     )
@@ -98,6 +64,33 @@ function SideBarComponent() {
                     Logout
                 </span>
             </button>
+        </div>
+    )
+}
+
+function BottomNavigation() {
+    return (
+        <div className='dashboard-bottom-navbar'>
+            <Link to={'/dashboard'}>
+                <span class="material-symbols-outlined" id='sidebar-icon'>
+                    dashboard
+                </span>
+            </Link>
+            <Link to={'new-job'}>
+                <span class="material-symbols-outlined" id='sidebar-icon'>
+                    post_add
+                </span>
+            </Link>
+            <Link>
+                <span class="material-symbols-outlined" id='sidebar-icon'>
+                    format_list_bulleted
+                </span>
+            </Link>
+            <Link>
+                <span class="material-symbols-outlined" id='sidebar-icon'>
+                    account_circle
+                </span>
+            </Link>
         </div>
     )
 }
