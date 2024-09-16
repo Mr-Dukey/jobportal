37990a66f7234daa26c061316fa30e3cdbc2ccd9
HARIHARAN.M <harimahe.1317@gmail.com>
2024-09-16 10:24:53 +0530

    dashboard

--- src/Components/CompanyMainDashboard/CompanyDashboard.jsx
+++ src/Components/CompanyMainDashboard/CompanyDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import './CompanyDashboard.css';
 import { motion } from 'framer-motion';
 import { Link, Outlet } from 'react-router-dom';
