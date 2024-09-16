0f3dd851156e2aed2fec5bf11dcec578326622f6
HARIHARAN.M <harimahe.1317@gmail.com>
2024-09-16 23:15:26 +0530

    post and get jobs

--- src/Components/New Job/NewJob.jsx
+++ src/Components/New Job/NewJob.jsx
@@ -1,104 +1,201 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import './NewJob.css'
-import { Col, Row } from 'react-bootstrap'
+import { Col, Row } from 'react-bootstrap';
+import { motion } from 'framer-motion';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function NewJob() {
-    
+    const navi = useNavigate();
+    const intialData = {
+        JobTitle: "",
+        JobType: "",
+        JobCompany: "",
+        JobLocation: "",
+        JobSalary: {
+            min_salary: 0,
+            max_salary: 0
+        },
+        JobQualification: "",
+        JobExperience: "",
+        JobDescription: "",
+        JobSkills: "",
+        JobPostedDate: new Date().toLocaleString({ hourCycle: 'h12' })
+    }
+
+    function jobReducer(state, action) {
+        if (action.type === 'change_text') {
+            return {
+                ...state,
+                [action.field]: action.payload
+            }
+        }
+        else if (action.type === 'change_minsalary') {
+            return {
+                ...state,
+                JobSalary: {
+                    ...state.JobSalary,
+                    min_salary: action.min_salary
+                }
+            };
+        }
+        else if (action.type === 'change_maxsalary') {
+            return {
+                ...state,
+                JobSalary: {
+                    ...state.JobSalary,
+                    max_salary: action.max_salary
+                }
+            };
+        }
+        else {
+            return false;
+        }
+    }
+    const [state, dispatch] = useReducer(jobReducer, intialData);
+
+    function handleJobChangeText(e) {
+        const { name, value } = e.target;
+
+        dispatch({
+            type: 'change_text',
+            field: name,
+            payload: value
+        })
+    }
+
+    function handleSubmitJobForm(e){
+        e.preventDefault();
+        console.log(state);
+        
+        axios.post('http://localhost:2400/job/post-a-job',state)
+        .then(()=>{
+            navi('/dashboard/new-job');
+        })
+        .catch(error=>console.log(error))
+    }
+
+
     return (
-        <div className='new-job-page'>
-            <div className="new-job-form">
-                <div className="new-job-sections">
-                    <Row className="row wholeform g-md-4 g-2" >
-                        <h5 style={{ height: '50px', textAlign: 'left' }}>Job Information:</h5>
-                        
-                        <Col xs={12} md={6}>
-                            <div className="new-job-field">
-                                <label htmlFor="job-title">Job Title</label>
-                                <div className="new-job-input">
-                                    <input type="text" placeholder='Enter Job Title...' name="" id="job-title" />
-                                </div>
-                            </div>
-                        </Col>
-                        <Col xs={12} md={6}>
-                            <div className="new-job-field">
-                                <label htmlFor="job-type">Job Type</label>
-                                <div className="new-job-input">
-                                    <select name="" id="job-type">
-                                        <option value="Full Time">Full&nbsp;Time</option>
-                                        <option value="Part Time">Part&nbsp;Time</option>
-                                        <option value="Work from Home">Work&nbsp;from&nbsp;Home</option>
-                                        <option value="Intern">Intern</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </Col>
-                        <Col xs={12} md={6}>
-                            <div className="new-job-field">
-                                <label htmlFor="job-company">Company</label>
-                                <div className="new-job-input ">
-                                    <input type="text" placeholder='Enter Your Company Name' name="" id="job-company" />
-                                </div>
-                            </div>
-                        </Col>
-                        <Col xs={12} md={6}>
-                            <div className="new-job-field">
-                                <label htmlFor="job-location">Location</label>
-                                <div className="new-job-input ">
-                                    <input type="text" placeholder='Enter Your Location' name="" id="job-location" />
-                                </div>
-                            </div>
-                        </Col>
-                        <Col xs={12} md={6}>
-                            <div className="new-job-field">
-                                <label htmlFor="job-salary">Salary</label>
-                                <div className="new-job-input ">
-                                    <input type="text" className='new-job-salary' placeholder='Min-Salary' name="" id="job-salary" />
-                                    <input type="text" className='new-job-salary' placeholder='Max-Salary' name="" id="" />
-                                </div>
-                            </div>
-                        </Col>
-                        <Col xs={12} md={6}>
-                            <div className="new-job-field">
-                                <label htmlFor="job-skill">Skills</label>
-                                <div className="new-job-input ">
-                                    <input type="text" placeholder='Enter Your Skills' name="" id="job-skill" />
-                                </div>
-                            </div>
-                        </Col>
-                        <Col xs={12} md={6}>
-                            <div className="new-job-field">
-                                <label htmlFor="job-qualification">Qualification</label>
-                                <div className="new-job-input ">
-                                    <input type="text" name="" id="job-qualification" placeholder='Enter job qualification' />
-                                </div>
-                            </div>
-                        </Col>
-                        <Col xs={12} md={6}>
-                            <div className="new-job-field">
-                                <label htmlFor="job-experience">Experience</label>
-                                <div className="new-job-input">
-                                    <input type="number" name="" id="job-experience" placeholder='Enter Experience' />
-                                </div>
-                            </div>
-                        </Col>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+        >
+            <div className='new-job-page'>
+                <div className="new-job-form">
+                    <div className="new-job-sections">
+                        <Row className="row wholeform g-md-4 g-2" >
+                            <h5 style={{ height: '50px', textAlign: 'left' }}>Job Information:</h5>
 
-                        <div className='description'>
-                        <h6 style={{ fontSize: '14px', textAlign: "left" }}>Job Description</h6>
-                            <textarea  placeholder='Type a job description...' rows="10" cols="105"></textarea>
-                        </div>
+                            <Col xs={12} md={6}>
+                                <div className="new-job-field">
+                                    <label htmlFor="job-title">Job Title</label>
+                                    <div className="new-job-input">
+                                        <input type="text" placeholder='Enter Job Title...' name="JobTitle" onChange={handleJobChangeText} value={state.JobTitle} id="job-title" />
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <div className="new-job-field">
+                                    <label htmlFor="job-type">Job Type</label>
+                                    <div className="new-job-input">
+                                        <select name="JobType" onChange={handleJobChangeText} value={state.JobType} id="job-type">
+                                            <option value="Full Time" selected>Full&nbsp;Time</option>
+                                            <option value="Part Time">Part&nbsp;Time</option>
+                                            <option value="Work from Home">Work&nbsp;from&nbsp;Home</option>
+                                            <option value="Intern">Intern</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <div className="new-job-field">
+                                    <label htmlFor="job-company">Company</label>
+                                    <div className="new-job-input ">
+                                        <input type="text" placeholder='Enter Your Company Name' onChange={handleJobChangeText} value={state.JobCompany} name="JobCompany" id="job-company" />
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <div className="new-job-field">
+                                    <label htmlFor="job-location">Location</label>
+                                    <div className="new-job-input ">
+                                        <input type="text" placeholder='Enter Your Location' name="JobLocation" onChange={handleJobChangeText} value={state.JobLocation} id="job-location" />
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <div className="new-job-field">
+                                    <label htmlFor="job-salary">Salary</label>
+                                    <div className="new-job-input ">
+                                        <input type="number" className='new-job-salary' placeholder='Min-Salary' 
+                                        onChange={(e)=>{
+                                            dispatch({
+                                                type:'change_minsalary',
+                                                min_salary:e.target.value
+                                            })
+                                        }} 
+                                        value={state.JobSalary.min_salary} name="" id="job-salary" />
+                                        <input type="number" className='new-job-salary' placeholder='Max-Salary'
+                                        onChange={(e)=>{
+                                            dispatch({
+                                                type:'change_maxsalary',
+                                                max_salary:e.target.value
+                                            })
+                                        }} 
+                                        value={state.JobSalary.max_salary}
+                                        name="" id="" />
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <div className="new-job-field">
+                                    <label htmlFor="job-skill">Skills</label>
+                                    <div className="new-job-input ">
+                                        <input type="text" placeholder='Enter Your Skills' onChange={handleJobChangeText} value={state.JobSkills} name="JobSkills" id="job-skill" />
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <div className="new-job-field">
+                                    <label htmlFor="job-qualification">Qualification</label>
+                                    <div className="new-job-input ">
+                                        <input type="text" name="JobQualification" id="job-qualification" onChange={handleJobChangeText} value={state.JobQualification} placeholder='Enter job qualification' />
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <div className="new-job-field">
+                                    <label htmlFor="job-experience">Experience</label>
+                                    <div className="new-job-input">
+                                        <input type="number" name="JobExperience" id="job-experience" onChange={handleJobChangeText} value={state.JobExperience} placeholder='Enter Experience' />
+                                    </div>
+                                </div>
+                            </Col>
+
+                            <div className='description'>
+                                <h6 style={{ fontSize: '14px', textAlign: "left" }}>Job Description</h6>
+                                <textarea name='JobDescription' placeholder='Type a job description...' onChange={handleJobChangeText} value={state.JobDescription} rows="10" cols="105"></textarea>
+                            </div>
 
 
 
-                        <div className="postnow">
-                            <button>Cancel</button>
-                            <button>Post Now</button>
-                        </div>
+                            <div className="postnow">
+                                <button 
+                                    onClick={()=>{
+                                        navi('/dashboard');
+                                    }}
+                                >Cancel</button>
+                                <button onClick={handleSubmitJobForm}>Post Now</button>
+                            </div>
 
 
-                    </Row>
+                        </Row>
+                    </div>
                 </div>
-            </div>
 
-        </div>
+            </div>
+        </motion.div>
     )
 }
--- src/Components/ViewPostedJobs/ViewPostedJobs.jsx
+++ src/Components/ViewPostedJobs/ViewPostedJobs.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ViewPostedJobs.css'
+import axios from 'axios';
 
 export default function ViewPostedJobs() {
+    const [jobViews, setJobiews] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:2400/job/get-all-jobs')
+            .then((res) => {
+                setJobiews(res.data)
+            })
+            .catch(error => console.log(error))
+    }, [jobViews])
     return (
         <div className='view-posted-job-page'>
             <table className="view-posted-box">
@@ -16,18 +26,25 @@ export default function ViewPostedJobs() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1.</td>
-                        <td>Software Engineer</td>
-                        <td>2-3</td>
-                        <td>HTML,...</td>
-                        <td>17/12/2002</td>
-                        <td>
-                            <button className='job-action-btn job-action-btn1'>View</button>
-                            <button className='job-action-btn job-action-btn2'>Edit</button>
-                            <button className='job-action-btn job-action-btn3'>Delete</button>
-                        </td>
-                    </tr>
+                    {
+                        jobViews.map((items, index) => {
+                            const postDate = new Date(items.JobPostedDate).toLocaleDateString(); // Properly format the date
+                            return (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{items.JobTitle}</td>
+                                    <td>{items.JobExperience}</td>
+                                    <td>{items.JobSkills}</td>
+                                    <td>{postDate}</td> {/* Render the formatted date */}
+                                    <td>
+                                        <button className='job-action-btn job-action-btn1'>View</button>
+                                        <button className='job-action-btn job-action-btn2'>Edit</button>
+                                        <button className='job-action-btn job-action-btn3'>Delete</button>
+                                    </td>
+                                </tr>
+                            );
+                        })
+                    }
                 </tbody>
             </table>
         </div>
