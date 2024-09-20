fc979cd14689e0aaf446d8000edf2b9cf062ec5b
HARIHARAN.M <harimahe.1317@gmail.com>
2024-09-20 21:06:14 +0530

    Routing update

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -12,15 +12,14 @@ export default function Routing() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Navigate to={'/dashboard/new-job'} />} />
-        {/* <Route path='register' Component={RegisterPage} /> */}
+        <Route path='/' element={<Navigate to={'/login'} />} />
         <Route path='register' Component={RegisterPage1} />
         <Route path='login' Component={LoginPage} />
 
         <Route path='/dashboard' Component={CompanyDashboard}>
-          <Route path='new-job' Component={NewJob} />
-          <Route path='posted-jobs' Component={ViewPostedJobs} />
-          <Route path='edit-job/:id' Component={EditJobDetails} />
+          <Route path='/dashboard/new-job' Component={NewJob} />
+          <Route path='/dashboard/posted-jobs' Component={ViewPostedJobs} />
+          <Route path='/dashboard/edit-job/:id' Component={EditJobDetails} />
         </Route>
         <Route path='/home' Component={ViewOpenings} />
 
